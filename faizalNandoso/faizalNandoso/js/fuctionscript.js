ef01fd1b49ab1d44e54cb00530ce46ee199485c6
LearniLm <faizal_ansari@hotmail.com>
2015-11-26 12:54:56 +1300

    This is straight after moving to VS

---
+++ faizalNandoso/faizalNandoso/js/fuctionscript.js
@@ -0,0 +1,14 @@
+function orderOnline()
+{
+	
+	
+	alert("This feature has not yet been implemented, please check back soon!");
+}
+
+
+function carreers()
+{
+	
+	
+	alert("This feature has not yet been implemented, we will be hiring soon!");
+}
