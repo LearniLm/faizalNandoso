af5150f50bb1960c57478630bee7099f0555e416
LearniLm <faizal_ansari@hotmail.com>
2015-11-27 00:08:57 +1300

    fixed up errors V8

---
+++ faizalNandoso/faizalNandoso/Controllers/commentsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using faizalNandoso.Models;
+
+namespace faizalNandoso.Controllers
+{
+    public class commentsController : ApiController
+    {
+        private faizalNandosoContext db = new faizalNandosoContext();
+
+        // GET: api/comments
+        public IQueryable<comments> Getcomments()
+        {
+            return db.comments;
+        }
+
+        // GET: api/comments/5
+        [ResponseType(typeof(comments))]
+        public IHttpActionResult Getcomments(int id)
+        {
+            comments comments = db.comments.Find(id);
+            if (comments == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(comments);
+        }
+
+        // PUT: api/comments/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putcomments(int id, comments comments)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != comments.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(comments).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!commentsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/comments
+        [ResponseType(typeof(comments))]
+        public IHttpActionResult Postcomments(comments comments)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.comments.Add(comments);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = comments.ID }, comments);
+        }
+
+        // DELETE: api/comments/5
+        [ResponseType(typeof(comments))]
+        public IHttpActionResult Deletecomments(int id)
+        {
+            comments comments = db.comments.Find(id);
+            if (comments == null)
+            {
+                return NotFound();
+            }
+
+            db.comments.Remove(comments);
+            db.SaveChanges();
+
+            return Ok(comments);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool commentsExists(int id)
+        {
+            return db.comments.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ faizalNandoso/faizalNandoso/Models/comments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace faizalNandoso.Models
+{
+    public class comments
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Comment { get; set; }
+        public int UserName { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<String> Comments { get; set; }
+    }
+}
